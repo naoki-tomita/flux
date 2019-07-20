a6a2ba74135d58d5259220cb25440f54fcc3c174
naoki.tomita <mxcpy608@hotmail.com>
2019-07-21 00:07:19 +0900

    First commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, createContext, FunctionComponent, useContext } from 'react';
 
-const App: React.FC = () => {
+function useCounter() {
+  const [count, setCount] = useState<number>(0);
+  return {
+    count,
+    increment: () => setCount(count + 1),
+    decrement: () => setCount(count - 1),
+  };
+}
+
+const CounterContext = createContext<{
+  count: number;
+  increment: () => void;
+  decrement: () => void
+}>({
+  count: 0,
+  increment: () => {},
+  decrement: () => {}
+});
+
+const Component1: FunctionComponent = () => {
+  const { count, increment } = useContext(CounterContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {count}
+      <button onClick={increment}>+</button>
     </div>
   );
 }
 
-export default App;
+const Component2: FunctionComponent = () => {
+  const { count, decrement } = useContext(CounterContext);
+  return (
+    <div>
+      {count}
+      <button onClick={decrement}>-</button>
+    </div>
+  );
+}
+
+export const App: FunctionComponent = () => {
+  const counter = useCounter();
+  return (
+    <CounterContext.Provider value={counter}>
+      <Component1 />
+      <Component2 />
+    </CounterContext.Provider>
+  );
+}
